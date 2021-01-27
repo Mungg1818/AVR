4d1c80756f06fa591e2f9f8d2a8a9d38a0d9bced
Pascal Weder <pasc.weder@gmx.ch>
2021-01-27 18:09:31 +0100

    MOD: Ignorelist build folder added MOD: CMake adjustement to toolchain.cmake ADD: toolchain.cmake

---
+++ _Project/toolchain.cmake
@@ -0,0 +1,12 @@
+#[[
+	Filename: /home/pi/Documents/AVR/_Project/toolchain.cmake
+    Path: /home/pi/Documents/AVR/_Project
+    Created Date: Wednesday, January 27th 2021, 5:30:19 pm
+	Author: Pascal Weder
+
+	Copyright (c) 2021 CodeHard AG
+]]#
+
+set(CMAKE_C_COMPILER "/usr/bin/avr-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/avr-g++")
+set(CMAKE_ASM_COMPILER "/usr/bin/avr-gcc")
