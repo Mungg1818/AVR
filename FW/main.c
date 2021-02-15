58fbeef116b8e26cc60bf1d5e2967fbeee06efda
Pascal Weder <pasc.weder@gmx.ch>
2021-02-15 09:56:11 +0100

    MOD: HelloWorld enhanced

--- FW/main.c
+++ FW/main.c
@@ -1,3 +1,27 @@
-int main(){
-    return 0;
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void) {
+
+   DDRD |= (1 << PD4);
+
+   while(1) {
+       PORTD ^= (1 << PD4);
+       _delay_ms(500);
+   }
+
+   return 0;
 }
