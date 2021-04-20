4dbb7ea6d2329fca5f525351feae46312b08d020
Pascal Weder <pasc.weder@gmx.ch>
2021-04-20 17:51:25 +0200

    ADD: .vscode/c_cpp_properties.json automatic header generation MOD: FW/CMakeLists.txt adjustment make environment MOD: FW/main.c 1 ms tick implemented and 1 s LED blink MOD: README.md enhanced

--- FW/main.c
+++ FW/main.c
@@ -11,17 +11,80 @@
 
 #define F_CPU 8000000UL
 
+#include <stdint.h>
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+// #include <util/delay.h>
 
-int main (void) {
+/**
+ * @brief 
+ * 
+ */
+volatile uint32_t ulTick = 0;
+volatile uint8_t ucMsTickEvent = 0u;
 
-   DDRD |= (1 << PD4);
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main(void)
+{
+    static uint32_t ulMsTickCount = 0u;
 
-   while(1) {
-       PORTD ^= (1 << PD4);
-       _delay_ms(500);
-   }
+    clock_prescale_set(clock_div_1);
 
-   return 0;
+    DDRD |= (1 << PD4 | 1 << PD5 | 1 << PD6 | 1 << PD7);
+    PORTD ^= (1 << PD7);
+
+    // TCCR0B |= (1 << CS01);
+    // TIMSK0 |= (1 << TOIE0);
+
+    OCR1AH = 0x1F;
+    OCR1AL = 0x40;
+    TCCR1B |= (1 << WGM12 | 1 << CS00);
+    TIMSK1 |= (1 << OCIE1A);
+
+    sei();
+
+    while (1)
+    {
+        if (0u != ucMsTickEvent)
+        {
+            ucMsTickEvent = 0u;
+            ulMsTickCount++;
+            if (0 == ulMsTickCount % 500u)
+            {
+                PORTD ^= (1 << PD4);
+                // usLed = 0u;
+            }
+            // if (usLed >= 2u)
+            // {
+            //     PORTD ^= (1 << PD5);
+            // }
+            // if (ulTick >= 0x10000){
+            //     PORTD ^= (1 << PD6);
+            // }
+        }
+    }
+
+    return 0u;
+}
+
+/**
+ * @brief Construct a new ISR object
+ * 
+ */
+ISR(TIMER1_COMPA_vect)
+{
+    // ulTick++;
+    // if (0 == (ulTick % 4))
+    // {
+        ucMsTickEvent = 0x01u;
+    // }
+    // if (0 == ulTick % 500)
+    // {
+    //     PORTD ^= (1 << PD4);
+    // }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AVR
 Learnig repository for git/AVR/CMake/Ninja/...
+
+## Programming
+1. sudo dfu-programmer at90usb1287 erase
+2. sudo dfu-programmer at90usb1287 flash HelloWorld.hex
